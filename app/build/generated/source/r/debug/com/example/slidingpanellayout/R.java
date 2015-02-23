f511772a34ad9c79dbec6dd561af88b97be582a2
hunt <hunt.b.graham@gmail.com>
2015-02-23 01:49:38 -0500

    Custom slider class to disable swiping, doesn't work yet.

--- app/build/generated/source/r/debug/com/example/slidingpanellayout/R.java
+++ app/build/generated/source/r/debug/com/example/slidingpanellayout/R.java
@@ -20,30 +20,33 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
-        public static final int closeButton=0x7f080000;
+        public static final int closeButton=0x7f080001;
         public static final int fragment_firstpane=0x7f080002;
         public static final int fragment_secondpane=0x7f080003;
         public static final int openButton=0x7f080004;
-        public static final int slidingpanelayout=0x7f080001;
+        public static final int slidingpanelayout=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_first_pane=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_pane_test=0x7f030002;
-        public static final int activity_second_pane=0x7f030003;
+        public static final int activity_custom_pane_test=0x7f030000;
+        public static final int activity_first_pane=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_pane_test=0x7f030003;
+        public static final int activity_second_pane=0x7f030004;
     }
     public static final class menu {
         public static final int first_pane=0x7f070000;
         public static final int main=0x7f070001;
-        public static final int pane_test=0x7f070002;
-        public static final int second_pane=0x7f070003;
+        public static final int menu_custom_pane_test=0x7f070002;
+        public static final int pane_test=0x7f070003;
+        public static final int second_pane=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
         public static final int hello_world=0x7f050002;
-        public static final int title_activity_first_pane=0x7f050003;
-        public static final int title_activity_second_pane=0x7f050004;
+        public static final int title_activity_custom_pane_test=0x7f050003;
+        public static final int title_activity_first_pane=0x7f050004;
+        public static final int title_activity_second_pane=0x7f050005;
     }
     public static final class style {
         /** 
